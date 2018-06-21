b4815d546c2217aac47767baca39a6b3521f7469
Richard Huang <flyerhzm@gmail.com>
2018-06-21 11:09:32 +0800

    do not call reset_concurrent_restriction if job is not a Restriction

--- lib/resque/plugins/job.rb
+++ lib/resque/plugins/job.rb
@@ -7,11 +7,13 @@ module Resque
         return unless payload = Resque.peek(queue)
         job_class = Object.const_get(payload['class'])
 
-        # hold if it reaches restriction.
-        return if job_class.respond_to?(:restriction_settings) && !job_class.restriction_settings.empty? && job_class.reach_restriction?(*payload['args'])
-        job_class.reset_concurrent_restriction(*payload['args'])
-
-        origin_reserve(queue)
+        if job_class.is_a?(Resque::Plugins::Restriction)
+          return if !job_class.restriction_settings.empty? && job_class.reach_restriction?(*payload['args'])
+          job_class.reset_concurrent_restriction(*payload['args'])
+          origin_reserve(queue)
+        else
+          origin_reserve(queue)
+        end
       end
     end
   end
