4b410d3d949a1c99bc052b6ac269a88fa141422c
Awesome Code <team@xinminlabs.com>
2018-10-09 02:03:21 +0000

    Auto corrected by following Ruby Space

--- lib/resque/plugins/restriction.rb
+++ lib/resque/plugins/restriction.rb
@@ -4,11 +4,11 @@ module Resque
       SECONDS = {
         :per_second => 1,
         :per_minute => 60,
-        :per_hour => 60*60,
-        :per_day => 24*60*60,
-        :per_week => 7*24*60*60,
-        :per_month => 31*24*60*60,
-        :per_year => 366*24*60*60
+        :per_hour => 60 * 60,
+        :per_day => 24 * 60 * 60,
+        :per_week => 7 * 24 * 60 * 60,
+        :per_month => 31 * 24 * 60 * 60,
+        :per_year => 366 * 24 * 60 * 60
       }
       RESTRICTION_QUEUE_PREFIX = 'restriction'
 
@@ -16,7 +16,7 @@ module Resque
         @options ||= {}
       end
 
-      def restrict(options={})
+      def restrict(options = {})
         settings.merge!(options)
       end
 
@@ -30,7 +30,7 @@ module Resque
           # first try to set period key to be the total allowed for the period
           # if we get a 0 result back, the key wasn't set, so we know we are
           # already tracking the count for that period'
-          period_active = ! Resque.redis.setnx(key, number.to_i - 1)
+          period_active = !Resque.redis.setnx(key, number.to_i - 1)
           # If we are already tracking that period, then decrement by one to
           # see if we are allowed to run, pushing to restriction queue to run
           # later if not.  Note that the value stored is the number of outstanding
@@ -42,7 +42,7 @@ module Resque
             if value < 0
               # reincrement the keys if one of the periods triggers DontPerform so
               # that we accurately track capacity
-              keys_decremented.each {|k| Resque.redis.incrby(k, 1) }
+              keys_decremented.each { |k| Resque.redis.incrby(k, 1) }
               Resque.push restriction_queue_name, :class => to_s, :args => args
               raise Resque::Job::DontPerform
             end
